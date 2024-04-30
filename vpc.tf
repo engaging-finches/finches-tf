4db25731ddd20e670f064812fbff14142d2aed84
meher-liatrio <meher.lippmann@liatrio.com>
2024-04-30 11:11:27 -0700

    feat: adds tf modules to create vpc and ec2 instance

---
+++ vpc.tf
@@ -0,0 +1,37 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.18.1"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.3.0"
+
+  count = 1
+  name  = "meher-ec2-cluster-${count.index}"
+
+  ami                    = "ami-04e5276ebb8451442"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "test"
+    Owner       = "Meher"
+    Client      = "Internal"
+    Project     = "DOB"
+    Appliocaiton = "app_server"
+  }
+}
